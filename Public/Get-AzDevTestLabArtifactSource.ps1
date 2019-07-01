74c7ec6ba17ee6a78c6ed834bde2d497197e741b
Adam Bertram <adbertram@gmail.com>
2019-07-01 14:31:10 -0500

    artifact source fix

--- Public/Get-AzDevTestLabArtifactSource.ps1
+++ Public/Get-AzDevTestLabArtifactSource.ps1
@@ -1,45 +1,44 @@
 function Get-AzDevTestLabArtifactSource {
-	[CmdletBinding()]
-	param
-	(
-		[Parameter()]
-		[ValidateNotNullOrEmpty()]
-		[string]$Name,
+    [CmdletBinding()]
+    param
+    (
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name,
 
-		[Parameter(Mandatory)]
-		[ValidateNotNullOrEmpty()]
-		[string]$LabName,
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$LabName,
 
-		[Parameter(Mandatory)]
-		[ValidateNotNullOrEmpty()]
-		[string]$ResourceGroupName,
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$ResourceGroupName,
 
-		[Parameter()]
-		[ValidateNotNullOrEmpty()]
-		[string]$ArmApiVersion = '2016-05-15'
-	)
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$ArmApiVersion = '2016-05-15'
+    )
 
-	$ErrorActionPreference = 'Stop'
+    $ErrorActionPreference = 'Stop'
 
-	try {
+    try {
 		
-		$getRepoParams = @{
-			ResourceGroupName = $ResourceGroupName
-			ResourceType      = 'Microsoft.DevTestLab/labs/artifactsources'
-			ResourceName      = $LabName
-			ApiVersion        = $ArmApiVersion
+        $getRepoParams = @{
+            ResourceGroupName = $ResourceGroupName
+            ResourceType      = 'Microsoft.DevTestLab/labs/artifactsources'
+            ResourceName      = $LabName
+            ApiVersion        = $ArmApiVersion
 
-		}
-		Get-AzResource @getRepoParams | Where-Object { $_.Name -eq $Name }
+        }
 
-		$whereFilter = { '*' }
-		if ($PSBoundParameters.ContainsKey('Name')) {
-			$whereFilter = { $_.Name -eq $Name }
-		}
-		Get-AzResource @getArtParams | Where-Object -FilterScript $whereFilter
+        $whereFilter = { '*' }
+        if ($PSBoundParameters.ContainsKey('Name')) {
+            $whereFilter = { $_.Name -eq $Name }
+        }
+        Get-AzResource @getArtParams | Where-Object -FilterScript $whereFilter
 		
-	} catch {
-		throw $_
-	}
+    } catch {
+        throw $_
+    }
 }
 
